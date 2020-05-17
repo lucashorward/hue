8516c5583d306f00a2387d723faeb6ae6c2b6f44
lucashorward <lucas.horward@gmail.com>
2020-05-17 21:41:52 +0200

    Added group control (CLI only)

--- hue-cli/src/main.rs
+++ hue-cli/src/main.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 #[tokio::main]
 async fn main() {
     if Path::new("./config.toml").exists() == false {
-        std::process::exit( {
+        std::process::exit({
             eprintln!("Please create a config.toml in your running directory containing the ip and apikey properties.");
             1
         });
@@ -13,9 +13,10 @@ async fn main() {
     let base_uri: String = hue_core::generate_base_uri();
     let command = std::env::args().nth(1).expect("no command given");
     if command == "lights" {
-        show_light_names(&base_uri).await;
-    }
-    else if command == "light" {
+        show_lights(&base_uri).await;
+    } else if command == "groups" {
+        show_groups(&base_uri).await
+    } else if command == "light" {
         let id = std::env::args().nth(2).expect("no id given");
         let subcommand = std::env::args().nth(3).expect("no sub command given");
         if subcommand == "on" {
@@ -28,36 +29,65 @@ async fn main() {
             let brightness = std::env::args().nth(4).expect("no brightness given");
             set_brightness(&base_uri, id, brightness.parse::<i16>().unwrap()).await;
         }
+    } else if command == "group" {
+        let id = std::env::args().nth(2).expect("no id given");
+        let subcommand = std::env::args().nth(3).expect("no sub command given");
+        if subcommand == "on" {
+            turn_on_group(&base_uri, &id).await;
+        }
+        if subcommand == "off" {
+            turn_off_group(&base_uri, &id).await;
+        }
     } else if command == "help" {
         show_help();
-    }
-    else {
+    } else {
         println!("Unknown command: {}", command);
     }
 }
 
-async fn show_light_names(uri: &String) {
+async fn show_lights(uri: &String) {
     let mut result: HashMap<i16, hue_core::Light> = HashMap::new();
     match hue_core::get_lights(uri).await {
         Err(why) => println!("{}", why),
         Ok(data) => result = data,
     };
     for (key, value) in &result {
-        println!("id: {} - name: {} - on: {:?}", key, value.name, value.state.on.unwrap());
+        println!(
+            "id: {} - name: {} - on: {:?}",
+            key,
+            value.name,
+            value.state.on.unwrap()
+        );
+    }
+}
+
+async fn show_groups(uri: &String) {
+    let mut result: HashMap<i16, hue_core::Group> = HashMap::new();
+    match hue_core::get_groups(uri).await {
+        Err(why) => println!("{}", why),
+        Ok(data) => result = data,
+    };
+    for (key, value) in &result {
+        println!(
+            "id: {} - name: {} - on: {:?}",
+            key,
+            value.name,
+            value.action.on.unwrap()
+        );
     }
 }
 
 async fn turn_on_light(uri: &String, id: &String) {
     match hue_core::turn_on_light(uri, id.to_string()).await {
         Err(why) => println!("{}", why),
-        Ok(_) => ()
+        Ok(_) => (),
     };
 }
 
 async fn turn_off_light(uri: &String, id: &String) {
     match hue_core::turn_off_light(uri, id.to_string()).await {
         Err(why) => println!("{}", why),
-        Ok(_) => ()
+        Ok(_) => (),
     };
 }
 
@@ -66,11 +96,29 @@ fn show_help() {
     println!("`lights` - prints all lights with id, name and status (on/off)");
     println!("`light {{id}} on` - turns light {{id}} on");
     println!("`light {{id}} off` - turns light {{id}} off");
+    println!("`light {{id}} brightness {{brightness}}` - sets brightness of light {{id}} to {{brightness}}");
+    println!("`groups` - prints all groups with id, name and status (on/off)");
+    println!("`group {{id}} on` - turns group {{id}} on");
+    println!("`group {{id}} off` - turns group {{id}} off");
 }
 
 async fn set_brightness(uri: &String, id: String, brightness: i16) {
-    match hue_core::set_brightness(uri, id, brightness).await {
+    match hue_core::set_light_brightness(uri, id, brightness).await {
         Err(why) => println!("{}", why),
-        Ok(_) => ()
+        Ok(_) => (),
+    };
+}
+
+async fn turn_on_group(uri: &String, id: &String) {
+    match hue_core::turn_on_group(uri, id.to_string()).await {
+        Err(why) => println!("{}", why),
+        Ok(_) => (),
+    };
+}
+
+async fn turn_off_group(uri: &String, id: &String) {
+    match hue_core::turn_off_group(uri, id.to_string()).await {
+        Err(why) => println!("{}", why),
+        Ok(_) => (),
     };
 }
--- hue-core/src/lib.rs
+++ hue-core/src/lib.rs
@@ -1,10 +1,10 @@
 extern crate config;
+extern crate reqwest;
 extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
-extern crate reqwest;
-use reqwest::Response;
 use reqwest::Error;
+use reqwest::Response;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
@@ -24,21 +24,30 @@ pub struct LightState {
 
 #[wasm_bindgen]
 impl LightState {
-    pub fn new(on: Option<bool>, bri: Option<i16>, hue: Option<i16>, sat: Option<i16>) -> LightState {
-        LightState { 
-            on,
-            bri,
-            hue,
-            sat
-        }
+    pub fn new(
+        on: Option<bool>,
+        bri: Option<i16>,
+        hue: Option<i16>,
+        sat: Option<i16>,
+    ) -> LightState {
+        LightState { on, bri, hue, sat }
     }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Light {
     pub state: LightState,
-    pub name: String
+    pub name: String,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Group {
+    pub name: String,
+    /// These are only the id's
+    pub lights: Vec<String>,
+    pub action: LightState,
+}
+
 pub fn generate_base_uri() -> String {
     let mut settings = config::Config::default();
     settings.merge(config::File::with_name("config")).unwrap();
@@ -57,26 +66,132 @@ pub async fn get_lights(base_uri: &String) -> Result<HashMap<i16, Light>, Error>
     Ok(resp)
 }
 
-pub async fn set_light_status(base_uri: &String, id: String, status: &LightState) -> Result<Response, Error> {
-    let post_string = format!("lights/{}/state", id);
+pub async fn get_groups(base_uri: &String) -> Result<HashMap<i16, Group>, Error> {
+    let api_string = create_api_url(&base_uri, &"groups".to_owned());
+    let resp = reqwest::get(&api_string)
+        .await?
+        .json::<HashMap<i16, Group>>()
+        .await?;
+    Ok(resp)
+}
+
+/// Sets status of a light or group
+/// Postfix is due to the API using the same object with different names (status for lights and action for groups) in a PUT call
+pub async fn set_object_status(
+    base_uri: &String,
+    id: String,
+    prefix: String,
+    postfix: String,
+    status: &LightState,
+) -> Result<Response, Error> {
+    let post_string = format!("{}/{}/{}", prefix, id, postfix);
     let api_string = create_api_url(&base_uri, &post_string);
     let client = reqwest::Client::new();
-    client.put(&api_string)
-        .json(status)
-        .send()
-        .await
+    client.put(&api_string).json(status).send().await
+}
+
+pub async fn set_light_status(
+    base_uri: &String,
+    id: String,
+    status: &LightState,
+) -> Result<Response, Error> {
+    set_object_status(
+        base_uri,
+        id,
+        "lights".to_owned(),
+        "status".to_owned(),
+        status,
+    )
+    .await
+}
+
+pub async fn set_group_action(
+    base_uri: &String,
+    id: String,
+    status: &LightState,
+) -> Result<Response, Error> {
+    set_object_status(
+        base_uri,
+        id,
+        "groups".to_owned(),
+        "action".to_owned(),
+        status,
+    )
+    .await
 }
 
 pub async fn turn_on_light(base_uri: &String, id: String) -> Result<Response, Error> {
-    set_light_status(base_uri, id, &LightState {on: Some(true), bri: None, hue: None, sat: None}).await
+    set_light_status(
+        base_uri,
+        id,
+        &LightState {
+            on: Some(true),
+            bri: None,
+            hue: None,
+            sat: None,
+        },
+    )
+    .await
 }
 
 pub async fn turn_off_light(base_uri: &String, id: String) -> Result<Response, Error> {
-    set_light_status(base_uri, id, &LightState {on: Some(false), bri: None, hue: None, sat: None}).await
+    set_light_status(
+        base_uri,
+        id,
+        &LightState {
+            on: Some(false),
+            bri: None,
+            hue: None,
+            sat: None,
+        },
+    )
+    .await
 }
 
-pub async fn set_brightness(base_uri: &String, id: String, brightness: i16) -> Result<Response, Error> {
-    set_light_status(base_uri, id, &LightState {on: None, bri: Some(brightness), hue: None, sat: None}).await
+pub async fn turn_on_group(base_uri: &String, id: String) -> Result<Response, Error> {
+    set_group_action(
+        base_uri,
+        id,
+        &LightState {
+            on: Some(true),
+            bri: None,
+            hue: None,
+            sat: None,
+        },
+    )
+    .await
+}
+
+pub async fn turn_off_group(base_uri: &String, id: String) -> Result<Response, Error> {
+    set_group_action(
+        base_uri,
+        id,
+        &LightState {
+            on: Some(false),
+            bri: None,
+            hue: None,
+            sat: None,
+        },
+    )
+    .await
+}
+
+pub async fn set_light_brightness(
+    base_uri: &String,
+    id: String,
+    brightness: i16,
+) -> Result<Response, Error> {
+    set_light_status(
+        base_uri,
+        id,
+        &LightState {
+            on: None,
+            bri: Some(brightness),
+            hue: None,
+            sat: None,
+        },
+    )
+    .await
 }
 
 fn create_base_uri(uri: &String, api_key: &String) -> String {
